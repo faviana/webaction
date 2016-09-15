a67f8efcb66388e4bf9962fbc0aba3d04e3497c3
Jason Skipper <jason.skipper@theironyard.com>
2016-09-15 08:18:04 -0400

    Base project ready

--- src/main/java/com/ironyard/servlets/MyFirstServlet.java
+++ src/main/java/com/ironyard/servlets/MyFirstServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by jasonskipper on 9/14/16.
@@ -16,6 +17,10 @@ public class MyFirstServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        // Actual logic goes here.
+        PrintWriter out = response.getWriter();
+        out.println("<h1>" + System.currentTimeMillis() + "</h1>");
 
     }
 }
